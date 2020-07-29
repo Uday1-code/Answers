84f3a305cea6b9e9fc643a69a21e6ad29435f60e
uday pspk <udaykumar90599@gmail.com>
2020-07-29 19:33:51 +0530

    emprate

---
+++ emprate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+ispresent=1
+random=$(( $RANDOM%2 ))
+emprateperhr=20;
+ emphrs=8;
+if [[ $ispresent -eq $random ]]
+then
+         emphrs=8;
+
+else
+        salary=0
+fi
+        salary=$(( $emprateperhr*$emphrs))
+
+
+
+
