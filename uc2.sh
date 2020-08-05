113256989cf598adbc5806e3e64fc086dde050b7
uday pspk <udaykumar90599@gmail.com>
2020-08-05 18:00:07 +0530

    day14uc2

---
+++ uc2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+shopt -s extglob
+echo "enter first name::"
+read word
+pat="^[A-z]{1}[a-z]{2}[ ][A-Z]{1}[a-z]{2}$"
+if [[ $word =~ $pat ]]
+then
+        echo "valid"
+else
+        echo "Invalid"
+fi
+
+
